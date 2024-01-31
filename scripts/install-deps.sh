50a8ae958d67c90035c5de1e866513cce79ef73d
duydq <duydq@rabiloo.com>
2024-01-31 16:19:36 +0700

    Update CI test and scripts

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-pip install black flake8 pytest
+pip install typing_extensions isort black flake8 pytest
 
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
 if [ -f requirements-dev.txt ]; then pip install -r requirements-dev.txt; fi
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,8 +2,9 @@
 
 set -eux
 
-# stop the build if there are Python syntax errors or undefined names
-flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+# exit-zero treats all errors as warnings.
+flake8 . --count --exit-zero --max-complexity=10 --max-line-length=120 --statistics --ignore W503
 
-# exit-zero treats all errors as warnings. The GitHub editor is 127 chars wide
-flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+black . --check --target-version=py310 --line-length=120
+
+isort . --check-only --profile=black --lbt=1 -l=120
